ee9e2da1b9c1b390a713e9fcb9987c0f4cd646fd
Lisandro Dalcin <dalcinl@gmail.com>
2009-02-26 15:42:25 +0000

    Make it easier to test builds using a fake "mpi.h" include file

--- misc/fakempi/mpi.h
+++ misc/fakempi/mpi.h
@@ -1,6 +1,14 @@
 #if !defined(PyMPI_FAKE_MPI_H)
 #define PyMPI_FAKE_MPI_H
 
-#warning "using a fake 'mpi.h' include file, just for testing!!!"
+#warning "Using a fake "mpi.h" include file, just for testing!!!"
+
+#if !defined(PyMPI_HAVE_CONFIG_H)
+#define PyMPI_HAVE_CONFIG_H
+#endif
+
+#if !defined(PyMPI_MPI_STATUSES_IGNORE_SIZE)
+#define PyMPI_MPI_STATUSES_IGNORE_SIZE 4096
+#endif
 
 #endif /*!PyMPI_FAKE_MPI_H */
