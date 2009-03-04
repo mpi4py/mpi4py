9f095544c875e83f1efa3fbc0aa13b03e87d6065
Lisandro Dalcin <dalcinl@gmail.com>
2009-03-04 20:31:31 +0000

    Simplify the implementation of the MPI-enabled Python executable, do not use any C stdlib call, just return -1 on MPI failures.

--- src/python.c
+++ src/python.c
@@ -12,106 +12,53 @@
 #define OMPI_IGNORE_CXX_SEEK
 #include <mpi.h>
 
+#define CHKIERR(ierr) if (ierr) return -1
 
-/* macro for MPI error checking */
-#define CHKIERR(MPI_CALL)          \
-  do {                             \
-    ierr = (MPI_CALL);             \
-    if (ierr != MPI_SUCCESS)       \
-      goto fail;                   \
-  } while(0)
+static int PyMPI_Main(int argc, char **argv)
+{
+  int sts=0, flag=0, ierr=0;
+
+  /* MPI Initalization */
+  ierr = MPI_Initialized(&flag); CHKIERR(ierr);
+  if (!flag) {
+#if 0 
+    int required = MPI_THREAD_MULTIPLE;
+    int provided = MPI_THREAD_SINGLE;
+    ierr = MPI_Init_thread(&argc, &argv, required, &provided); CHKIERR(ierr);
+#else
+    ierr = MPI_Init(&argc, &argv); CHKIERR(ierr);
+#endif
+  }
+
+  /* Python main */
+#if PY_MAJOR_VERSION < 3
+  sts = Py_Main(argc, argv);
+#else
+  #warning "this may not work Python 3 ..."
+  sts = Py_Main(argc, argv);
+#endif
+
+  /* MPI finalization */
+  ierr = MPI_Finalized(&flag); CHKIERR(ierr);
+  if (!flag) { 
+    ierr = MPI_Finalize(); CHKIERR(ierr);
+  }
+
+  /* return */
+  return sts;
+}
 
 
 int main(int argc, char **argv)
 {
-  int ierr   = 0; /* MPI error code       */
-  int status = 0; /* Python return status */
-
 #ifdef __FreeBSD__
-  /* 754 requires that FP exceptions run in "no stop" mode by default,
-   * and until C vendors implement C99's ways to control FP exceptions,
-   * Python requires non-stop mode.  Alas, some platforms enable FP
-   * exceptions by default.  Here we disable them.
-   */
   fp_except_t m;
   m = fpgetmask();
   fpsetmask(m & ~FP_X_OFL);
 #endif
-
-  /* -------------------------------------------------------------- */
-  /* Initialize MPI                                                 */
-  /* -------------------------------------------------------------- */
-  { /* Perhaps MPI is already initialized of finalized.
-     * I know, this does not make any sense ...                     */
-    int initialized=1, finalized=1;
-    MPI_Initialized(&initialized);
-    MPI_Finalized(&finalized);
-    if (!initialized && !finalized) {
-#if 1
-      CHKIERR( MPI_Init(&argc, &argv) );
-#else /* room for the future */
-      int required = MPI_THREAD_MULTIPLE;
-      int provided = MPI_THREAD_SINGLE;
-      CHKIERR( MPI_Init_thread(&argc, &argv, required, &provided) );
-#endif
-    }
-  }
-  /* -------------------------------------------------------------- */
-
-  /* -------------------------------------------------------------- */
-  /* Command Line Arguments                                         */
-  /* -------------------------------------------------------------- */
-  /* XXX I should broadcast command line arguments here !!!         */
-  /* -------------------------------------------------------------- */
-
-  /* -------------------------------------------------------------- */
-  /* Python main                                                    */
-  /* -------------------------------------------------------------- */
-  status = Py_Main(argc, argv);
-  /* -------------------------------------------------------------- */
-
-  /* -------------------------------------------------------------- */
-  /* Finalize MPI                                                   */
-  /* -------------------------------------------------------------- */
-  { /* Perhaps some previous call (from Python) finalized MPI.
-     * Should we inform about that? It is not good practice ...     */
-    int initialized=1, finalized=1;
-    MPI_Initialized(&initialized);
-    MPI_Finalized(&finalized);
-    if (initialized && !finalized) CHKIERR( MPI_Finalize() );
-  }
-  /* -------------------------------------------------------------- */
-
-  return status;
-
-  /* -------------------------------------------------------------- */
-  /* Handle MPI error                                               */
-  /* -------------------------------------------------------------- */
- fail:
-  {
-    char msgstr[MPI_MAX_ERROR_STRING+1] = { '\0' };
-    int msglen = 0;
-    int initialized = 0, finalized = 1;
-    MPI_Initialized(&initialized);
-    MPI_Finalized(&finalized);
-    if (initialized && !finalized) {
-      MPI_Error_string(ierr, msgstr, &msglen);
-    } else {
-      strncpy(msgstr, "unknown", MPI_MAX_ERROR_STRING);
-    }
-    msgstr[MPI_MAX_ERROR_STRING] = '\0';
-    fflush(stderr);
-    fprintf(stderr, "\n"
-	    "MPI_Init()/MPI_Finalize() failed\n. "
-	    "error code: %d, error string: %s"
-	    "\n", ierr, msgstr);
-    fflush(stderr);
-    exit(ierr);
-    return ierr;
-  }
-  /* -------------------------------------------------------------- */
-
+  return PyMPI_Main(argc, argv);
 }
+
 
 /*
    Local variables:
