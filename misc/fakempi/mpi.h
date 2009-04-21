73ecc1e265a2e23b93366de2d2d48bab355b5190
Lisandro Dalcin <dalcinl@gmail.com>
2009-04-21 02:46:53 +0000

    Fix for MSVC and test builds using fake "mpi.h"

--- misc/fakempi/mpi.h
+++ misc/fakempi/mpi.h
@@ -1,7 +1,9 @@
 #if !defined(PyMPI_FAKE_MPI_H)
 #define PyMPI_FAKE_MPI_H
 
+#ifdef __GNUC__
 #warning "Using a fake "mpi.h" include file, just for testing!!!"
+#endif
 
 #if !defined(PyMPI_HAVE_CONFIG_H)
 #define PyMPI_HAVE_CONFIG_H
