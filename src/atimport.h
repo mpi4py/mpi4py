60d1a3694559a61936ae078b2ff165271c08f1de
Lisandro Dalcin <dalcinl@gmail.com>
2009-06-11 20:22:09 +0000

    GIL-init hackery for ancient Python no longer needed (Cython handles it)

--- src/atimport.h
+++ src/atimport.h
@@ -115,22 +115,10 @@ static PyObject * PyMPIString_AsStringAndSize(PyObject *ob,
 
 /* ---------------------------------------------------------------- */
 
-/* Really VILE HACK for Python < 2.4.2 !!
-   Ensure in advance that the GIL was created */
-
-#if PY_VERSION_HEX < 0x02040200 && defined(WITH_THREAD)
-#define PyImport_AddModule(module) \
-  (PyEval_InitThreads(), PyImport_AddModule(module))
-#endif
-
-
 /* Enable the block below if for any
    reason you want to disable threads */
-#if 0
 
-#if defined(PyImport_AddModule)
-#undef PyImport_AddModule
-#endif
+#if 0
 
 #define PyGILState_Ensure() (PyGILState_STATE)0)
 #define PyGILState_Release(state) (state)=(PyGILState_STATE)0)
