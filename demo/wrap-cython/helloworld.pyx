fe2df10121d329f41e75794e86da8f3f95f50a14
Lisandro Dalcin <dalcinl@gmail.com>
2009-10-02 17:49:10 +0000

    Add demo about wrapping with Cython

---
+++ demo/wrap-cython/helloworld.pyx
@@ -0,0 +1,21 @@
+cimport mpi4py.MPI as MPI
+from mpi4py.mpi_c cimport *
+
+cdef extern from "stdio.h":
+    int printf(char*, ...)
+
+cdef void c_sayhello(MPI_Comm comm):
+    cdef int size, rank, plen
+    cdef char pname[MPI_MAX_PROCESSOR_NAME]
+    if comm == MPI_COMM_NULL:
+        printf("You passed MPI_COMM_NULL !!!\n")
+        return
+    MPI_Comm_size(comm, &size)
+    MPI_Comm_rank(comm, &rank)
+    MPI_Get_processor_name(pname, &plen)
+    printf("Hello, World! I am process %d of %d on %s.\n",
+           rank, size, pname)
+
+def sayhello(MPI.Comm comm not None ):
+    cdef MPI_Comm c_comm = comm.ob_mpi
+    c_sayhello(c_comm)
---
+++ demo/wrap-cython/test.py
@@ -0,0 +1,22 @@
+from mpi4py import MPI
+import helloworld as hw
+
+null = MPI.COMM_NULL
+hw.sayhello(null)
+
+comm = MPI.COMM_WORLD
+hw.sayhello(comm)
+
+try:
+    hw.sayhello(None)
+except:
+    pass
+else:
+    assert 0, "exception not raised"
+
+try:
+    hw.sayhello(list())
+except:
+    pass
+else:
+    assert 0, "exception not raised"
