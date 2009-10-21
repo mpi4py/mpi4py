47998672f3ecb949a4e91b9144f0afab93ff4bfd
Lisandro Dalcin <dalcinl@gmail.com>
2009-10-21 19:10:18 +0000

    Reworked dlopen-based hackery for Open MPI and dynamic loading issues

--- src/compat/openmpi.h
+++ src/compat/openmpi.h
@@ -1,7 +1,153 @@
 #ifndef PyMPI_COMPAT_OPENMPI_H
 #define PyMPI_COMPAT_OPENMPI_H
 
-/* ---------------------------------------------------------------- */
+/* ------------------------------------------------------------------------- */
+/* ------------------------------------------------------------------------- */
+
+/*
+ * The hackery below redefines the actuall calls to 'MPI_Init()' and
+ * 'MPI_Init_thread()' in order to preload the main MPI dynamic
+ * library with appropriate flags to 'dlopen()' ensuring global
+ * availability of library symbols.
+ */
+
+#ifndef OPENMPI_DLOPEN_LIBMPI
+#define OPENMPI_DLOPEN_LIBMPI 1
+#endif
+
+#if OPENMPI_DLOPEN_LIBMPI
+#if HAVE_DLOPEN
+
+#if HAVE_DLFCN_H
+  #include <dlfcn.h>
+#else
+  #if defined(__CYGWIN__)
+    #define RTLD_LAZY   1
+    #define RTLD_NOW    2
+    #define RTLD_LOCAL  0
+    #define RTLD_GLOBAL 4
+    #define RTLD_NOLOAD 0
+  #elif defined(__APPLE__)
+    #define RTLD_LAZY   0x1
+    #define RTLD_NOW    0x2
+    #define RTLD_LOCAL  0x4
+    #define RTLD_GLOBAL 0x8
+    #define RTLD_NOLOAD 0x10
+  #elif defined(__linux__)
+    #define RTLD_LAZY	0x00001
+    #define RTLD_NOW	0x00002
+    #define RTLD_LOCAL	0x00000
+    #define RTLD_GLOBAL	0x00100
+    #define RTLD_NOLOAD	0x00004
+  #endif
+  #if defined(c_plusplus) || defined(__cplusplus)
+  extern "C" {
+  #endif
+  #if !defined(OMPI_DECLSPEC)
+  #define OMPI_DECLSPEC extern
+  #endif
+  OMPI_DECLSPEC void *dlopen(const char *, int);
+  #if defined(c_plusplus) || defined(__cplusplus)
+  }
+  #endif
+#endif
+
+#ifndef RTLD_LAZY
+#define RTLD_LAZY 1
+#endif
+#ifndef RTLD_NOW
+#define RTLD_NOW RTLD_LAZY
+#endif
+#ifndef RTLD_LOCAL
+#define RTLD_LOCAL 0
+#endif
+#ifndef RTLD_GLOBAL
+#define RTLD_GLOBAL RTLD_LOCAL
+#endif
+#ifndef RTLD_NOLOAD
+#define RTLD_NOLOAD 0
+#endif
+
+/*
+static void * my_dlopen(const char *name, int mode) {
+  void *handle;
+  static int called = 0;
+  if (!called) {
+    called = 1;
+    #if HAVE_DLFCN_H
+    printf("HAVE_DLFCN_H: yes\n");
+    #else
+    printf("HAVE_DLFCN_H: no\n");
+    #endif
+    printf("\n");
+    printf("RTLD_LAZY:    0x%X\n", RTLD_LAZY   );
+    printf("RTLD_NOW:     0x%X\n", RTLD_NOW    );
+    printf("RTLD_LOCAL:   0x%X\n", RTLD_LOCAL  );
+    printf("RTLD_GLOBAL:  0x%X\n", RTLD_GLOBAL );
+    printf("RTLD_NOLOAD:  0x%X\n", RTLD_NOLOAD );
+    printf("\n");
+  }
+  handle = dlopen(name, mode);
+  printf("dlopen(\"%s\",0x%X) -> %p\n", name, mode, handle);
+  printf("dlerror() -> %s\n\n", dlerror());
+  return handle;
+}
+#define dlopen my_dlopen
+*/
+
+static void OPENMPI_dlopen_libmpi(void)
+{
+  int mode = RTLD_NOW | RTLD_GLOBAL | RTLD_NOLOAD;
+  void *handle = 0;
+#if defined(__CYGWIN__)
+  if (!handle)
+    handle = dlopen("cygmpi.dll", mode);
+  if (!handle)
+    handle = dlopen("mpi.dll", mode);
+#elif defined(__APPLE__)
+  /* Mac OS X */
+  if (!handle)
+    handle = dlopen("libmpi.1.dylib", mode);
+  if (!handle)
+    handle = dlopen("libmpi.0.dylib", mode);
+  if (!handle)
+    handle = dlopen("libmpi.dylib", mode);
+#else
+  /* GNU/Linux and others*/
+  if (!handle)
+    handle = dlopen("libmpi.so.1", mode);
+  if (!handle)
+    handle = dlopen("libmpi.so.0", mode);
+  if (!handle)
+    handle = dlopen("libmpi.so", mode);
+#endif
+}
+
+static int PyMPI_OPENMPI_MPI_Init(int *argc, char ***argv)
+{
+  OPENMPI_dlopen_libmpi();
+  return MPI_Init(argc, argv);
+}
+#undef  MPI_Init
+#define MPI_Init PyMPI_OPENMPI_MPI_Init
+
+static int PyMPI_OPENMPI_MPI_Init_thread(int *argc, char ***argv,
+                                         int required, int *provided)
+{
+  OPENMPI_dlopen_libmpi();
+  return MPI_Init_thread(argc, argv, required, provided);
+}
+#undef  MPI_Init_thread
+#define MPI_Init_thread PyMPI_OPENMPI_MPI_Init_thread
+
+#endif /* !HAVE_DLOPEN */
+#endif /* !OPENMPI_DLOPEN_LIBMPI */
+
+/* ------------------------------------------------------------------------- */
+/* ------------------------------------------------------------------------- */
+
+
+/* ------------------------------------------------------------------------- */
 
 #if (defined(OMPI_MAJOR_VERSION) && \
      defined(OMPI_MINOR_VERSION) && \
@@ -13,174 +159,7 @@
 #define PyMPI_OPENMPI_VERSION 10000
 #endif
 
-/* ---------------------------------------------------------------- */
-
-/*
- * Open MPI releases 1.2.4 and above are shipped with newer versions
- * of GNU libtool.  These libtool versions load dynamic libraries in
- * such a way that library symbols are not globally available by
- * default. This behavior do not interact well with some interal Open
- * MPI components, specifically the ones related to one-side
- * communication operations ('MPI_Win' and related). The vile hackery
- * below try to fix this issue by intercepting the calls to 'MPI_Init'
- * and 'MPI_Finalize' in order to manage preloading of the main Open
- * MPI dynamic library with appropriate flags enablig global
- * availability of library symbols.
- */
-
-#if !defined(OPENMPI_DLOPEN_LIBMPI)
-#if PyMPI_OPENMPI_VERSION < 10204
-#define OPENMPI_DLOPEN_LIBMPI 0
-#else
-#define OPENMPI_DLOPEN_LIBMPI 1
-#endif
-#endif /* !defined(OPENMPI_DLOPEN_LIBMPI) */
-
-
-#if OPENMPI_DLOPEN_LIBMPI
-
-#if PyMPI_OPENMPI_VERSION < 10400
-
-#if defined(__cplusplus)
-#define LT_BEGIN_C_DECLS extern "C" {
-#define LT_END_C_DECLS }
-#else
-#define LT_BEGIN_C_DECLS
-#define LT_END_C_DECLS
-#endif
-#if defined(OMPI_DECLSPEC)
-#define LT_SCOPE OMPI_DECLSPEC
-#else
-#define LT_SCOPE extern
-#endif
-LT_BEGIN_C_DECLS
-typedef void *lt_dlhandle;
-typedef void *lt_dladvise;
-LT_SCOPE int         lt_dlinit           (void);
-LT_SCOPE int         lt_dlexit           (void);
-LT_SCOPE int         lt_dladvise_init    (lt_dladvise *advise);
-LT_SCOPE int         lt_dladvise_destroy (lt_dladvise *advise);
-LT_SCOPE int         lt_dladvise_ext     (lt_dladvise *advise);
-LT_SCOPE int         lt_dladvise_global  (lt_dladvise *advise);
-LT_SCOPE int         lt_dladvise_local   (lt_dladvise *advise);
-LT_SCOPE lt_dlhandle lt_dlopenadvise     (const char *filename,
-                                          lt_dladvise advise);
-LT_SCOPE int         lt_dlclose          (lt_dlhandle handle);
-LT_END_C_DECLS
-
-#elif defined(HAVE_LTDL_H)
-
-#include <ltdl.h>
-
-#elif defined(HAVE_DLOPEN)
-
-#ifdef HAVE_DLFCN_H
-#include <dlfcn.h>
-#endif
-#ifndef RTLD_LAZY
-#define RTLD_LAZY 1
-#endif
-#ifndef RTLD_NOW
-#define RTLD_NOW RTLD_LAZY
-#endif
-#define lt_dlhandle            void*
-#define lt_dladvise            int
-#define lt_dlinit()            (0)
-#define lt_dlexit()            (0)
-#define lt_dladvise_init(a)    (*(a)=RTLD_NOW,0)
-#define lt_dladvise_destroy(a) (*(a)=0,0)
-#define lt_dladvise_ext(a)     (*(a)|=0,0)
-#define lt_dladvise_global(a)  (*(a)|=RTLD_GLOBAL,0)
-#define lt_dladvise_local(a)   (*(a)|=RTLD_LOCAL,0)
-#define lt_dlopenadvise(n,a)   dlopen(n,a)
-#define lt_dlclose(h)          dlclose(h)
-
-#endif
-
-static int         OPENMPI_ltdlup = 0;
-static lt_dlhandle OPENMPI_libmpi = 0;
-
-static void OPENMPI_dlopen_libmpi(void)
-{
-  int         ltdlup = OPENMPI_ltdlup;
-  lt_dlhandle libmpi = OPENMPI_libmpi;
-  if (!ltdlup)
-    ltdlup = (lt_dlinit() == 0);
-  if (ltdlup) {
-    lt_dladvise advise;
-    (void)lt_dladvise_init(&advise);
-    (void)lt_dladvise_global(&advise);
-    if (!libmpi) /* GNU/Linux */
-      libmpi = lt_dlopenadvise("libmpi.so.1", advise);
-    if (!libmpi)
-      libmpi = lt_dlopenadvise("libmpi.so.0", advise);
-    if (!libmpi)
-      libmpi = lt_dlopenadvise("libmpi.so", advise);
-    if (!libmpi) /* Mac OS X */
-      libmpi = lt_dlopenadvise("libmpi.1.dylib", advise);
-    if (!libmpi)
-      libmpi = lt_dlopenadvise("libmpi.0.dylib", advise);
-    if (!libmpi)
-      libmpi = lt_dlopenadvise("libmpi.dylib", advise);
-    (void)lt_dladvise_ext(&advise);
-    if (!libmpi) /* Others */
-      libmpi = lt_dlopenadvise("libmpi", advise);
-    (void)lt_dladvise_destroy(&advise);
-  }
-  OPENMPI_libmpi = libmpi;
-  OPENMPI_ltdlup = ltdlup;
-}
-
-static void OPENMPI_dlclose_libmpi(void)
-{
-  int         ltdlup = OPENMPI_ltdlup;
-  lt_dlhandle libmpi = OPENMPI_libmpi;
-  if (libmpi && ltdlup) {
-    (void)lt_dlclose(libmpi);
-    libmpi = 0;
-  }
-  if (ltdlup) {
-    (void)lt_dlexit();
-    ltdlup = 0;
-  }
-  OPENMPI_libmpi = libmpi;
-  OPENMPI_ltdlup = ltdlup;
-}
-
-static int PyMPI_OPENMPI_MPI_Init(int *argc, char ***argv)
-{
-  int ierr = MPI_SUCCESS;
-  OPENMPI_dlopen_libmpi();
-  ierr = MPI_Init(argc, argv);
-  return ierr;
-}
-#undef  MPI_Init
-#define MPI_Init PyMPI_OPENMPI_MPI_Init
-
-static int PyMPI_OPENMPI_MPI_Init_thread(int *argc, char ***argv,
-                                         int required, int *provided)
-{
-  int ierr = MPI_SUCCESS;
-  OPENMPI_dlopen_libmpi();
-  ierr = MPI_Init_thread(argc, argv, required, provided);
-  return ierr;
-}
-#undef  MPI_Init_thread
-#define MPI_Init_thread PyMPI_OPENMPI_MPI_Init_thread
-
-static int PyMPI_OPENMPI_MPI_Finalize(void)
-{
-  int ierr = MPI_SUCCESS;
-  ierr = MPI_Finalize();
-  OPENMPI_dlclose_libmpi();
-  return ierr;
-}
-#undef  MPI_Finalize
-#define MPI_Finalize PyMPI_OPENMPI_MPI_Finalize
-
-#endif /* !OPENMPI_DLOPEN_LIBMPI */
-
-/* ---------------------------------------------------------------- */
+/* ------------------------------------------------------------------------- */
 
 /*
  * Open MPI < 1.1.3 generates an error when MPI_File_get_errhandler()
@@ -204,7 +183,7 @@ static int PyMPI_OPENMPI_Errhandler_free(MPI_Errhandler *errhandler)
 
 #endif /* !(PyMPI_OPENMPI_VERSION < 10103) */
 
-/* ---------------------------------------------------------------- */
+/* ------------------------------------------------------------------------- */
 
 /*
  * Open MPI 1.1 generates an error when MPI_File_get_errhandler() is
@@ -260,7 +239,7 @@ static MPI_Fint PyMPI_OPENMPI_File_c2f(MPI_File file)
 
 #endif /* !(PyMPI_OPENMPI_VERSION < 10301) */
 
-/* ---------------------------------------------------------------- */
+/* ------------------------------------------------------------------------- */
 
 #if PyMPI_OPENMPI_VERSION < 10400
 
@@ -290,7 +269,7 @@ static int PyMPI_OPENMPI_Win_set_errhandler(MPI_Win win,
 
 #endif /* !(PyMPI_OPENMPI_VERSION < 10400) */
 
-/* ---------------------------------------------------------------- */
+/* ------------------------------------------------------------------------- */
 
 #endif /* !PyMPI_COMPAT_OPENMPI_H */
 
