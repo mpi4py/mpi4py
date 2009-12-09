fdcf368b63c81f8e4f8eab783a829df902ca596b
Lisandro Dalcin <dalcinl@gmail.com>
2009-12-09 18:40:53 +0000

    A leftover fix forgotten in previous commit

--- test/test_exceptions.py
+++ test/test_exceptions.py
@@ -49,7 +49,7 @@ class TestExcDatatype(unittest.TestCase):
 
 _name, _version = MPI.get_vendor()
 if _name == 'Open MPI':
-    if _version < (1, 4, 0):
+    if _version < (1, 4, 1):
         TestExcDatatype.DATATYPES = TestExcDatatype.DATATYPES[1:]
         TestExcDatatype.ERR_TYPE = MPI.ERR_INTERN
         TestExcDatatype.ERR_KEYVAL = MPI.ERR_OTHER
@@ -305,7 +305,7 @@ except NotImplementedError:
 else:
     _name, _version = MPI.get_vendor()
     if _name == 'Open MPI':
-        if _version < (1, 4, 0):
+        if _version < (1, 4, 1):
             TestExcWin.ERR_KEYVAL = MPI.ERR_OTHER
 
 
