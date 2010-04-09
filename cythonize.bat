c7a281409a97889d841587c68a0c413c9e8de699
Lisandro Dalcin <dalcinl@gmail.com>
2010-04-09 21:26:25 +0000

    Add convenience shell and batch scripts for running Cython on the source tree

---
+++ cythonize.bat
@@ -0,0 +1,4 @@
+@echo off
+python -m cython --cleanup 3 -w src -I include mpi4py.MPE.pyx
+python -m cython --cleanup 3 -w src -I include mpi4py.MPI.pyx
+move src\mpi4py.MPI*.h src\include\mpi4py
---
+++ cythonize.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+python -m cython --cleanup 3 -w src -Iinclude mpi4py.MPE.pyx && \
+python -m cython --cleanup 3 -w src -Iinclude mpi4py.MPI.pyx && \
+mv src/mpi4py.MPI*.h src/include/mpi4py
