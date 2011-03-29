81277e14b5c3c2a2731589e56cb1988f7b552416
Lisandro Dalcin <dalcinl@gmail.com>
2011-03-29 01:28:06 +0000

    Update fake mpi.h header

--- misc/fakempi/mpi.h
+++ misc/fakempi/mpi.h
@@ -12,5 +12,7 @@
 #define MPI_Finalize() 0
 #define MPI_Initialized(a) ((*(a)=1),0)
 #define MPI_Finalized(a) ((*(a)=1),0)
+#define MPI_COMM_WORLD 0
+#define MPI_Abort(a,b) 0
 
 #endif /*!PyMPI_FAKE_MPI_H */
