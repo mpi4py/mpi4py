e7376c4d1902ddbaa9e94d8c149e72edf04ca744
Lisandro Dalcin <dalcinl@gmail.com>
2011-04-04 02:55:48 +0000

    Tests: dont call Alloc_mem() with zero size

--- test/test_mem.py
+++ test/test_mem.py
@@ -2,9 +2,10 @@ import unittest
 from mpi4py import MPI
 
 class TestMemory(unittest.TestCase):
-
+    
     def testMemory1(self):
         for size in range(0, 10000, 100):
+            if size == 0: size = 1
             try:
                 mem1 = MPI.Alloc_mem(size)
                 self.assertEqual(len(mem1), size)
@@ -14,6 +15,7 @@ class TestMemory(unittest.TestCase):
 
     def testMemory2(self):
         for size in range(0, 10000, 100):
+            if size == 0: size = 1
             try:
                 mem2 = MPI.Alloc_mem(size, MPI.INFO_NULL)
                 self.assertEqual(len(mem2), size)
