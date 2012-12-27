ac3e9e170058101786385a0a0be223334be3ebb4
Lisandro Dalcin <dalcinl@gmail.com>
2012-12-27 14:13:42 -0300

    Fixes after testing with mpich-3.0.1

--- src/compat/mpich3.h
+++ src/compat/mpich3.h
@@ -1,4 +1,29 @@
 #ifndef PyMPI_COMPAT_MPICH3_H
 #define PyMPI_COMPAT_MPICH3_H
 
+#if defined(MPICH_NUMVERSION) && (MPICH_NUMVERSION < 30100000)
+
+static int PyMPI_MPICH3_MPI_Type_size_x(MPI_Datatype datatype, MPI_Count *size)
+{
+  int ierr = MPI_Type_commit(&datatype); if (ierr) return ierr;
+  return MPI_Type_size_x(datatype,size);
+}
+#define MPI_Type_size_x PyMPI_MPICH3_MPI_Type_size_x
+
+static int PyMPI_MPICH3_MPI_Type_get_extent_x(MPI_Datatype datatype, MPI_Count *lb, MPI_Count *extent)
+{
+  int ierr = MPI_Type_commit(&datatype); if (ierr) return ierr;
+  return MPI_Type_get_extent_x(datatype,lb,extent);
+}
+#define MPI_Type_get_extent_x PyMPI_MPICH3_MPI_Type_get_extent_x
+
+static int PyMPI_MPICH3_MPI_Type_get_true_extent_x(MPI_Datatype datatype, MPI_Count *lb, MPI_Count *extent)
+{
+  int ierr = MPI_Type_commit(&datatype); if (ierr) return ierr;
+  return MPI_Type_get_true_extent_x(datatype,lb,extent);
+}
+#define MPI_Type_get_true_extent_x PyMPI_MPICH3_MPI_Type_get_true_extent_x
+
+#endif
+
 #endif /* !PyMPI_COMPAT_MPICH3_H */
