d54fc56aa2cce20edacb0654436402817add5549
Lisandro Dalcin <dalcinl@gmail.com>
2014-04-08 13:32:40 +0300

    Travis-CI: Install missing dependency for MPICH3

--- conf/travis-install-mpi.sh
+++ conf/travis-install-mpi.sh
@@ -6,7 +6,7 @@ case $1 in
   mpich2) set -x;
     sudo apt-get install -q mpich2 libmpich2-3 libmpich2-dev;;
   mpich3) set -x;
-    sudo apt-get install -q gfortran libcr0;
+    sudo apt-get install -q gfortran libcr0 default-jdk;
     wget -q http://www.cebacad.net/files/mpich/ubuntu/mpich-3.1/mpich_3.1-1ubuntu_amd64.deb;
     sudo dpkg -i ./mpich_3.1-1ubuntu_amd64.deb;
     rm -f ./mpich_3.1-1ubuntu_amd64.deb;;
