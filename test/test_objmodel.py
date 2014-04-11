dd8f34a87b77f3317d8af7dd2bd54619f99aac89
Lisandro Dalcin <dalcinl@gmail.com>
2014-04-11 12:50:53 +0300

    Many additions to improve test coverage

---
+++ test/test_objmodel.py
@@ -0,0 +1,89 @@
+from mpi4py import MPI
+import mpiunittest as unittest
+
+
+class TestObjModel(unittest.TestCase):
+
+    objects = [
+        MPI.DATATYPE_NULL,
+        MPI.REQUEST_NULL,
+        MPI.INFO_NULL,
+        MPI.ERRHANDLER_NULL,
+        MPI.GROUP_NULL,
+        MPI.WIN_NULL,
+        MPI.OP_NULL,
+        MPI.FILE_NULL,
+        MPI.MESSAGE_NULL,
+        MPI.COMM_NULL,
+    ]
+
+    def testEq(self):
+        for i, obj1 in enumerate(self.objects):
+            objects = self.objects[:]
+            obj2 = objects[i]
+            self.assertTrue(obj1 == obj2)
+            del objects[i]
+            for obj2 in objects:
+                self.assertFalse(obj1 == obj2)
+                self.assertFalse(obj2 == obj1)
+            self.assertFalse(None == obj1 )
+            self.assertFalse(obj1 == None )
+            self.assertFalse(obj1 == True )
+            self.assertFalse(obj1 == False)
+            self.assertFalse(obj1 == 12345)
+            self.assertFalse(obj1 == "abc")
+            self.assertFalse(obj1 == [123])
+            self.assertFalse(obj1 == (1,2))
+            self.assertFalse(obj1 == {0:0})
+            self.assertFalse(obj1 == set())
+
+    def testNe(self):
+        for i, obj1 in enumerate(self.objects):
+            objects = self.objects[:]
+            obj2 = objects[i]
+            self.assertFalse(obj1 != obj2)
+            del objects[i]
+            for obj2 in objects:
+                self.assertTrue (obj1 != obj2)
+            self.assertTrue(None != obj1 )
+            self.assertTrue(obj1 != None )
+            self.assertTrue(obj1 != True )
+            self.assertTrue(obj1 != False)
+            self.assertTrue(obj1 != 12345)
+            self.assertTrue(obj1 != "abc")
+            self.assertTrue(obj1 != [123])
+            self.assertTrue(obj1 != (1,2))
+            self.assertTrue(obj1 != {0:0})
+            self.assertTrue(obj1 != set())
+
+    def testBool(self):
+        for obj in self.objects:
+            self.assertFalse(not not obj)
+            self.assertTrue(not obj)
+            self.assertFalse(obj)
+
+    def testHash(self):
+        for obj in self.objects:
+            ob_hash = lambda: hash(obj)
+            self.assertRaises(TypeError, ob_hash)
+
+    def testInit(self):
+        for i, obj in enumerate(self.objects):
+            klass = type(obj)
+            new = klass()
+            self.assertEqual(new, obj)
+            new = klass(obj)
+            self.assertEqual(new, obj)
+            objects = self.objects[:]
+            del objects[i]
+            for other in objects:
+                ob_init = lambda: klass(other)
+                self.assertRaises(TypeError, ob_init)
+            ob_init = lambda: klass(1234)
+            self.assertRaises(TypeError, ob_init)
+            ob_init = lambda: klass("abc")
+            self.assertRaises(TypeError, ob_init)
+
+
+if __name__ == '__main__':
+    unittest.main()
--- test/test_request.py
+++ test/test_request.py
@@ -12,11 +12,18 @@ class TestRequest(unittest.TestCase):
         self.REQUEST.Wait()
         self.REQUEST.Wait(None)
         self.REQUEST.Wait(self.STATUS)
+        self.REQUEST.wait()
+        self.REQUEST.wait(None)
+        self.REQUEST.wait(self.STATUS)
+
 
     def testTest(self):
         self.REQUEST.Wait()
         self.REQUEST.Wait(None)
         self.REQUEST.Test(self.STATUS)
+        self.REQUEST.wait()
+        self.REQUEST.wait(None)
+        self.REQUEST.test(self.STATUS)
 
     def testGetStatus(self):
         try: flag = self.REQUEST.Get_status()
@@ -42,17 +49,30 @@ class TestRequestArray(unittest.TestCase):
         MPI.Request.Waitany(self.REQUESTS)
         MPI.Request.Waitany(self.REQUESTS, None)
         MPI.Request.Waitany(self.REQUESTS, self.STATUSES[0])
+        MPI.Request.waitany(self.REQUESTS)
+        MPI.Request.waitany(self.REQUESTS, None)
+        MPI.Request.waitany(self.REQUESTS, self.STATUSES[0])
 
     def testTestany(self):
         MPI.Request.Testany(self.REQUESTS)
         MPI.Request.Testany(self.REQUESTS, None)
         MPI.Request.Testany(self.REQUESTS, self.STATUSES[0])
+        MPI.Request.testany(self.REQUESTS)
+        MPI.Request.testany(self.REQUESTS, None)
+        MPI.Request.testany(self.REQUESTS, self.STATUSES[0])
 
     def testWaitall(self):
         MPI.Request.Waitall(self.REQUESTS)
         MPI.Request.Waitall(self.REQUESTS, None)
+        for statuses in (tuple(self.STATUSES), (self.STATUSES[0],), ()):
+            MPI.Request.Waitall(self.REQUESTS, statuses)
         for statuses in (self.STATUSES, []):
             MPI.Request.Waitall(self.REQUESTS, statuses)
+            self.assertEqual(len(statuses), len(self.REQUESTS))
+        MPI.Request.waitall(self.REQUESTS)
+        MPI.Request.waitall(self.REQUESTS, None)
+        for statuses in (self.STATUSES, []):
+            MPI.Request.waitall(self.REQUESTS, statuses)
             self.assertEqual(len(statuses), len(self.REQUESTS))
 
     def testTestall(self):
@@ -60,6 +80,11 @@ class TestRequestArray(unittest.TestCase):
         MPI.Request.Testall(self.REQUESTS, None)
         for statuses in (self.STATUSES, []):
             MPI.Request.Testall(self.REQUESTS, statuses)
+            self.assertEqual(len(statuses), len(self.REQUESTS))
+        MPI.Request.testall(self.REQUESTS)
+        MPI.Request.testall(self.REQUESTS, None)
+        for statuses in (self.STATUSES, []):
+            MPI.Request.testall(self.REQUESTS, statuses)
             self.assertEqual(len(statuses), len(self.REQUESTS))
 
     def testWaitsome(self):
