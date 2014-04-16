1190ef3222ea03bfe05e25e8f9f5de764bd18e1d
Lisandro Dalcin <dalcinl@gmail.com>
2014-04-16 21:26:25 +0300

    Update year 2013 -> 2014

--- docs/source/usrman/conf.py
+++ docs/source/usrman/conf.py
@@ -55,7 +55,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'MPI for Python'
-copyright = u'2013, Lisandro Dalcin'
+copyright = u'2014, Lisandro Dalcin'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
