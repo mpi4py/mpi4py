441e247207bf4a29dcb790d132145daa61ea2482
Lisandro Dalcin <dalcinl@gmail.com>
2014-08-23 20:36:59 +0300

    test: Add tests for mpi4py.rc function

---
+++ test/test_rc.py
@@ -0,0 +1,23 @@
+from mpi4py import rc
+import mpiunittest as unittest
+
+class TestRC(unittest.TestCase):
+
+    def testRC1(self):
+        rc(initialize   = rc.initialize)
+        rc(threaded     = rc.threaded)
+        rc(thread_level = rc.thread_level)
+        rc(finalize     = rc.finalize)
+        rc(fast_reduce  = rc.fast_reduce)
+        rc(recv_mprobe  = rc.recv_mprobe)
+
+    def testRC2(self):
+        kwargs = rc.__dict__.copy()
+        rc(**kwargs)
+
+    def testRC3(self):
+        error = lambda: rc(ABCXYZ=123456)
+        self.assertRaises(TypeError, error)
+
+if __name__ == '__main__':
+    unittest.main()
