c546ad560fe1a64416c38b58223aa64c3da01473
Thomas Spura <thomas.spura@gmail.com>
2014-09-04 16:24:29 +0200

    Add initial jenkins-fedoracloud script

---
+++ conf/jenkins-fedoracloud.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+# Test script running on Fedora jenkins http://jenkins.cloud.fedoraproject.org/job/mpi4py
+# Copyright (c) 2014, Thomas Spura.
+
+#rpm -qa | sort
+
+if [ "$#" -ne 1 ]; then
+    echo "Usage: jenkins-fedoracloud.sh \$MPI_IMPLEMENTATION"
+    exit 1
+fi
+MPI=$1
+echo "Running tests with MPI: $MPI"
+# TODO check if this MPI implementation is available
+
+## define mpi_{un,}load
+source /etc/profile.d/modules.sh
+_mpi_load="module load mpi/$MPI-$(uname -m)"
+_mpi_unload="module purge"
+
+rm -rf mpi4pyenv_$MPI build
+virtualenv mpi4pyenv_$MPI
+source mpi4pyenv_$MPI/bin/activate
+
+pip install Cython
+pip install nose pylint --upgrade  ## Needed within the venv
+hash -r  ## Reload where the nosetests app is (within the venv) - see `which nosetests` with and without
+pip install nosexcover
+
+$_mpi_load
+
+make build
+
+# TODO check if default pep8, pylint and coverage reports are 'sane'
+coverage run test/runtests.py
+coverage xml
+
+mpiexec -np 5 python test/runtests.py
+mpiexec -np 8 python test/runtests.py
+
+$_mpi_unload
+
+pep8 demo src | tee pep8.out 
+pylint build/*/mpi4py | tee pylint.out
+
+deactivate
