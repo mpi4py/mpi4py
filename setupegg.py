41676a16cc6152a52793bc4fe6e943977c4f67f0
Lisandro Dalcin <dalcinl@gmail.com>
2015-05-18 03:38:53 +0300

    setup: Update open mode in setupegg.py

--- setupegg.py
+++ setupegg.py
@@ -3,5 +3,5 @@
 # Contact: dalcinl@gmail.com
 "Wrapper to run setup.py using setuptools"
 import setuptools
-with open('setup.py', 'rb') as f:
+with open('setup.py') as f:
     exec(compile(f.read(), 'setup.py', 'exec'))
