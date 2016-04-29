75ef8fd9d515ae1756f13dc2567a89e3f478267b
Lisandro Dalcin <dalcinl@gmail.com>
2016-04-29 11:33:49 +0300

    Travis-CI: Update configuration file to use Ubuntu Trusty and OS X

--- conf/travis/install-mpi.sh
+++ conf/travis/install-mpi.sh
@@ -1,17 +1,31 @@
 #!/bin/sh
 set -e
-case $1 in
-  mpich1) set -x;
-    sudo apt-get install -q mpich-shmem-bin libmpich-shmem1.0-dev;;
-  mpich2) set -x;
-    sudo apt-get install -q mpich2 libmpich2-3 libmpich2-dev;;
-  mpich3) set -x;
-    sudo apt-get install -q gfortran libcr0 default-jdk;
-    wget -q http://www.cebacad.net/files/mpich/ubuntu/mpich-3.1/mpich_3.1-1ubuntu_amd64.deb;
-    sudo dpkg -i ./mpich_3.1-1ubuntu_amd64.deb;
-    rm -f ./mpich_3.1-1ubuntu_amd64.deb;;
-  openmpi) set -x;
-    sudo apt-get install openmpi-bin openmpi-dev;;
-  *)
-    echo "Unknown MPI implementation:" $1; exit 1;;
+case `uname` in
+Linux)
+  sudo apt-get update -q
+  case $1 in
+    mpich) set -x;
+      sudo apt-get install -y -q mpich libmpich-dev
+      ;;
+    openmpi) set -x;
+      sudo apt-get install -y -q openmpi-bin libopenmpi-dev
+      ;;
+    *)
+      echo "Unknown MPI implementation:" $1
+      exit 1
+      ;;
+  esac
+  ;;
+#Darwin)
+#  brew update
+#  case $1 in
+#    mpich|openmpi) set -x;
+#      brew install $1
+#      ;;
+#    *)
+#      echo "Unknown MPI implementation:" $1
+#      exit 1
+#      ;;
+#  esac
+#  ;;
 esac
