f83f8bb4ab47215622a5cb76d654f96f17f0f381
Lisandro Dalcin <dalcinl@gmail.com>
2016-06-08 18:43:45 +0300

    conda: binary relocation for MPICH

--- conf/conda-recipes/mpich/build.sh
+++ conf/conda-recipes/mpich/build.sh
@@ -5,5 +5,11 @@ export CXX=${CXX-c++}
   --disable-fortran \
   --disable-dependency-tracking \
   --prefix=$PREFIX
+
+sedinplace() { [[ $(uname) == Darwin ]] && sed -i "" $@ || sed -i"" $@; }
+sedinplace s%--prefix=$PREFIX%--prefix=\$PREFIX%g src/include/mpichinfo.h
+sedinplace s%--prefix=$PREFIX%--prefix=\$PREFIX%g src/pm/hydra/include/hydra_config.h
+sedinplace s%-I$(dirname $SRC_DIR)%-I%g           src/pm/hydra/include/hydra_config.h
+
 make -j $CPU_COUNT
 make install
