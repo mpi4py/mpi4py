3452f9215c7feeffe3b5bcc70e0aa3736b6476fc
Lisandro Dalcin <dalcinl@gmail.com>
2017-02-09 19:48:14 +0300

    test: Disable mpi4py.futures tests for Open MPI v2.0.x

--- demo/futures/test_futures.py
+++ demo/futures/test_futures.py
@@ -815,6 +815,8 @@ class MPICommExecutorTest(unittest.TestCase):
 SKIP_POOL_TEST = False
 name, version = MPI.get_vendor()
 if name == 'Open MPI':
+    if version[:2] == (2,0):
+        SKIP_TEST = True
     if version < (1,8,0):
         SKIP_POOL_TEST = True
     if sys.platform.startswith('win'):
