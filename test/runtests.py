029bd2f3ed630e6a4bf4b467d597695e4a4127f9
Lisandro Dalcin <dalcinl@gmail.com>
2017-10-29 16:48:38 +0300

    test: Do not override default thread level from mpi4py.rc

--- test/runtests.py
+++ test/runtests.py
@@ -38,7 +38,7 @@ def getoptionparser():
                       help="initialize MPI without thread support")
     parser.add_option("--thread-level", type="choice",
                       choices=["single", "funneled", "serialized", "multiple"],
-                      action="store", dest="thread_level", default="multiple",
+                      action="store", dest="thread_level", default=None,
                       help="initialize MPI with required thread support")
     parser.add_option("--mpe",
                       action="store_true", dest="mpe", default=False,
@@ -100,8 +100,10 @@ def import_package(options, pkgname):
     package = __import__(pkgname)
     #
     import mpi4py.rc
-    mpi4py.rc.threads = options.threads
-    mpi4py.rc.thread_level = options.thread_level
+    if options.threads is not None:
+        mpi4py.rc.threads = options.threads
+    if options.thread_level is not None:
+        mpi4py.rc.thread_level = options.thread_level
     if options.mpe:
         mpi4py.profile('mpe', logfile='runtests-mpi4py')
     if options.vt:
