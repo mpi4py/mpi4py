71c126a1118618cef302fc26ff4a9c3a164a43d8
Lisandro Dalcin <dalcinl@gmail.com>
2019-02-15 13:50:13 +0300

    Update year 2017 -> 2019

--- docs/source/usrman/conf.py
+++ docs/source/usrman/conf.py
@@ -60,7 +60,7 @@ master_doc = 'index'
 # General information about the project.
 package = 'mpi4py'
 project = u'MPI for Python'
-copyright = u'2017, Lisandro Dalcin'
+copyright = u'2019, Lisandro Dalcin'
 author = u'Lisandro Dalcin'
 
 # The version info for the project you're documenting, acts as replacement for
