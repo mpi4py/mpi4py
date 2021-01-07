138e8f8e912e1b8cc2b070eb77170099dc9885b1
Lisandro Dalcin <dalcinl@gmail.com>
2021-01-07 22:05:31 +0300

    Update support for PETSc MPIUNI

--- conf/mpiuni/mpi.h
+++ conf/mpiuni/mpi.h
@@ -1,13 +1,20 @@
+/* Author:  Lisandro Dalcin   */
+/* Contact: dalcinl@gmail.com */
+
 #ifndef PyMPI_MPIUNI_H
 #define PyMPI_MPIUNI_H
 
 #include <petscconf.h>
-#undef  PETSC_HAVE_FORTRAN
+#undef PETSC_HAVE_HIP
+#undef PETSC_HAVE_CUDA
+#undef PETSC_HAVE_FORTRAN
 #include <petsc/mpiuni/mpi.h>
 
-#define __PETSCSYS_H
+#define PETSCSYS_H
+#define PETSCIMPL_H
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <../src/sys/mpiuni/mpi.c>
 #include <../src/sys/mpiuni/mpitime.c>
 
