2b10fa6444024b57e9e5203e2e69a143552a9cb6
Lisandro Dalcin <dalcinl@gmail.com>
2021-07-01 14:32:00 +0300

    MPI-4: Add config/compat headers to support MPI-4 implementations

--- src/lib-mpi/config/mpich.h
+++ src/lib-mpi/config/mpich.h
@@ -1,5 +1,5 @@
-#ifndef PyMPI_CONFIG_MPICH3_H
-#define PyMPI_CONFIG_MPICH3_H
+#ifndef PyMPI_CONFIG_MPICH_H
+#define PyMPI_CONFIG_MPICH_H
 
 #include "mpi-11.h"
 #include "mpi-12.h"
@@ -7,13 +7,14 @@
 #include "mpi-22.h"
 #include "mpi-30.h"
 #include "mpi-31.h"
+#include "mpi-40.h"
 
 /* These types are difficult to implement portably */
 #undef PyMPI_HAVE_MPI_REAL2
 #undef PyMPI_HAVE_MPI_COMPLEX4
 
 #ifndef ROMIO_VERSION
-#include "mpich3-io.h"
+#include "mpi-io.h"
 #endif
 
-#endif /* !PyMPI_CONFIG_MPICH3_H */
+#endif /* !PyMPI_CONFIG_MPICH_H */
