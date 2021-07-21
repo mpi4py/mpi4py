d2f68a9bdf65532ec0ae91963a1ab1b06cd8b345
Leo Fang <leofang@bnl.gov>
2021-07-21 08:41:30 -0400

    Temporarily revert the theme

--- docs/source/usrman/conf.py
+++ docs/source/usrman/conf.py
@@ -101,10 +101,10 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-#html_theme = (
-#    'sphinx_rtd_theme' if 'sphinx_rtd_theme' in extensions else 'default'
-#)
-html_theme = 'pydata_sphinx_theme'
+html_theme = (
+    'sphinx_rtd_theme' if 'sphinx_rtd_theme' in extensions else 'default'
+)
+#html_theme = 'pydata_sphinx_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
