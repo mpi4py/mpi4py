abdfde0820ea9114a97c52d2e0ec61cb1a58289f
Lisandro Dalcin <dalcinl@gmail.com>
2021-08-10 17:02:59 +0300

    readthedocs: Minor fixes and tweaks to sphinx-rtd-theme

--- docs/source/usrman/conf.py
+++ docs/source/usrman/conf.py
@@ -166,7 +166,10 @@ if html_theme == 'sphinx_rtd_theme':
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
-html_static_path = []
+
+html_css_files = [
+    'css/custom.css',
+]
 
 
 # -- Options for HTMLHelp output ------------------------------------------
