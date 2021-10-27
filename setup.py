e895878a569c43bb0fefa82c5a31dee6f2af65cd
Lisandro Dalcin <dalcinl@gmail.com>
2021-10-27 21:44:22 +0300

    setup: Update classifiers

--- setup.py
+++ setup.py
@@ -69,10 +69,7 @@ Operating System :: Unix
 Programming Language :: C
 Programming Language :: Cython
 Programming Language :: Python
-Programming Language :: Python :: 2
-Programming Language :: Python :: 2.7
 Programming Language :: Python :: 3
-Programming Language :: Python :: 3.5
 Programming Language :: Python :: 3.6
 Programming Language :: Python :: 3.7
 Programming Language :: Python :: 3.8
