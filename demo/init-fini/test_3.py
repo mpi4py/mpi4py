ef695b7a1eed1f1320503f09db752e5324fb4dc8
Lisandro Dalcin <dalcinl@gmail.com>
2022-05-10 19:27:16 +0300

    mpi4py.rc: Use new utility function for str -> bool conversion

--- demo/init-fini/test_3.py
+++ demo/init-fini/test_3.py
@@ -1,13 +1,18 @@
 import os
 from mpi4py import rc
 
-assert rc.initialize is True
-assert rc.finalize   is None
-os.environ['MPI4PY_RC_INITIALIZE'] = 'false'
-os.environ['MPI4PY_RC_FINALIZE']   = 'off'
-from mpi4py import MPI
-assert rc.initialize is False
-assert rc.finalize   is False
+assert rc.initialize   is True
+assert rc.finalize     is None
+assert rc.thread_level == 'multiple'
 
+os.environ['MPI4PY_RC_INITIALIZE']   = 'false'
+os.environ['MPI4PY_RC_FINALIZE']     = 'off'
+os.environ['MPI4PY_RC_THREAD_LEVEL'] = 'single'
+
+from mpi4py import MPI
 assert not MPI.Is_initialized()
 assert not MPI.Is_finalized()
+
+assert rc.initialize   is False
+assert rc.finalize     is False
+assert rc.thread_level == 'single'
