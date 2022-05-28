045c4a4a4fbb5204e6f6a16e6da46822e49da7c8
Lisandro Dalcin <dalcinl@gmail.com>
2022-05-28 14:08:09 +0300

    test: Refactor Errhandler tests

--- test/test_errhandler.py
+++ test/test_errhandler.py
@@ -13,109 +13,117 @@ class TestErrhandler(unittest.TestCase):
         else:
             self.assertFalse(MPI.ERRORS_ABORT)
 
-    def testGetErrhandler(self):
+class BaseTestErrhandler:
+
+    def testCall(self):
+        mpiobj = self.mpiobj
+        mpiobj.Set_errhandler(MPI.ERRORS_RETURN)
+        try:
+            mpiobj.Call_errhandler(MPI.SUCCESS)
+            mpiobj.Call_errhandler(MPI.ERR_OTHER)
+        except NotImplementedError:
+            if MPI.VERSION >= 2: raise
+            clsname = type(mpiobj).__name__.lower()
+            self.skipTest(f'mpi-{clsname}-call-errhandler')
+
+    def testGetFree(self):
+        mpiobj = self.mpiobj
         errhdls = []
         for i in range(100):
-            e = MPI.COMM_WORLD.Get_errhandler()
+            e = mpiobj.Get_errhandler()
             errhdls.append(e)
         for e in errhdls:
             e.Free()
         for e in errhdls:
             self.assertEqual(e, MPI.ERRHANDLER_NULL)
 
-    def testSessionGetSetErrhandler(self):
+    def _run_test_get_set(self, errhandler):
+        mpiobj = self.mpiobj
+        errhdl_1 = mpiobj.Get_errhandler()
+        self.assertNotEqual(errhdl_1, MPI.ERRHANDLER_NULL)
+        mpiobj.Set_errhandler(errhandler)
+        errhdl_2 = mpiobj.Get_errhandler()
+        self.assertEqual(errhdl_2, errhandler)
+        errhdl_2.Free()
+        self.assertEqual(errhdl_2, MPI.ERRHANDLER_NULL)
+        mpiobj.Set_errhandler(errhdl_1)
+        errhdl_1.Free()
+        self.assertEqual(errhdl_1, MPI.ERRHANDLER_NULL)
+
+    def testErrorsReturn(self):
+        self._run_test_get_set(MPI.ERRORS_RETURN)
+
+    def testErrorsFatal(self):
+        self._run_test_get_set(MPI.ERRORS_ARE_FATAL)
+
+    @unittest.skipUnless(MPI.ERRORS_ABORT, 'mpi-errors-abort')
+    @unittest.skipMPI("mpich(<4.1.0)")
+    def testErrorsAbort(self):
+        self._run_test_get_set(MPI.ERRORS_ABORT)
+
+
+class TestErrhandlerComm(BaseTestErrhandler, unittest.TestCase):
+
+    def setUp(self):
+        self.mpiobj = MPI.COMM_SELF.Dup()
+
+    def tearDown(self):
+        self.mpiobj.Free()
+
+
+class TestErrhandlerWin(BaseTestErrhandler, unittest.TestCase):
+
+    def setUp(self):
         try:
-            session = MPI.Session.Init()
-        except NotImplementedError:
-            self.skipTest('mpi-session')
-        for ERRHANDLER in [
-            MPI.ERRORS_ARE_FATAL,
-            MPI.ERRORS_RETURN,
-            MPI.ERRORS_ARE_FATAL,
-            MPI.ERRORS_RETURN,
-        ]:
-            errhdl_1 = session.Get_errhandler()
-            self.assertNotEqual(errhdl_1, MPI.ERRHANDLER_NULL)
-            session.Set_errhandler(ERRHANDLER)
-            errhdl_2 = session.Get_errhandler()
-            self.assertEqual(errhdl_2, ERRHANDLER)
-            errhdl_2.Free()
-            self.assertEqual(errhdl_2, MPI.ERRHANDLER_NULL)
-            session.Set_errhandler(errhdl_1)
-            errhdl_1.Free()
-            self.assertEqual(errhdl_1, MPI.ERRHANDLER_NULL)
-        session.Finalize()
-
-    def testCommGetSetErrhandler(self):
-        for COMM in [MPI.COMM_SELF, MPI.COMM_WORLD]:
-            for ERRHANDLER in [
-                MPI.ERRORS_ARE_FATAL,
-                MPI.ERRORS_RETURN,
-                MPI.ERRORS_ARE_FATAL,
-                MPI.ERRORS_RETURN,
-            ]:
-                errhdl_1 = COMM.Get_errhandler()
-                self.assertNotEqual(errhdl_1, MPI.ERRHANDLER_NULL)
-                COMM.Set_errhandler(ERRHANDLER)
-                errhdl_2 = COMM.Get_errhandler()
-                self.assertEqual(errhdl_2, ERRHANDLER)
-                errhdl_2.Free()
-                self.assertEqual(errhdl_2, MPI.ERRHANDLER_NULL)
-                COMM.Set_errhandler(errhdl_1)
-                errhdl_1.Free()
-                self.assertEqual(errhdl_1, MPI.ERRHANDLER_NULL)
-
-    def testSessionCallErrhandler(self):
-        try:
-            session = MPI.Session.Init()
-        except NotImplementedError:
-            self.skipTest('mpi-session')
-        session.Set_errhandler(MPI.ERRORS_RETURN)
-        session.Call_errhandler(MPI.ERR_OTHER)
-        session.Finalize()
-
-    @unittest.skipMPI('MPI(<2.0)')
-    def testCommCallErrhandler(self):
-        errhdl = MPI.COMM_SELF.Get_errhandler()
-        comm = MPI.COMM_SELF.Dup()
-        comm.Set_errhandler(MPI.ERRORS_RETURN)
-        comm.Call_errhandler(MPI.ERR_OTHER)
-        comm.Free()
-
-    @unittest.skipMPI('MPI(<2.0)')
-    @unittest.skipMPI('SpectrumMPI')
-    def testWinCallErrhandler(self):
-        try:
-            win = MPI.Win.Create(MPI.BOTTOM, 1, MPI.INFO_NULL, MPI.COMM_SELF)
+            self.mpiobj = MPI.Win.Create(MPI.BOTTOM, 1, MPI.INFO_NULL, MPI.COMM_SELF)
         except NotImplementedError:
             self.skipTest('mpi-win')
-        win.Set_errhandler(MPI.ERRORS_RETURN)
-        win.Call_errhandler(MPI.ERR_OTHER)
-        win.Free()
+        except MPI.Exception:
+            self.skipTest('mpi-win')
 
-    @unittest.skipMPI('MPI(<2.0)')
-    @unittest.skipMPI('msmpi')
-    def testFileCallErrhandler(self):
+    def tearDown(self):
+        self.mpiobj.Free()
+
+    @unittest.skipMPI('SpectrumMPI')
+    def testCall(self):
+        super().testCall()
+
+
+class TestErrhandlerFile(BaseTestErrhandler, unittest.TestCase):
+
+    def setUp(self):
         import os, tempfile
         rank = MPI.COMM_WORLD.Get_rank()
         fd, filename = tempfile.mkstemp(prefix='mpi4py-', suffix="-%d"%rank)
         os.close(fd)
         amode = MPI.MODE_WRONLY | MPI.MODE_CREATE | MPI.MODE_DELETE_ON_CLOSE
         try:
-            file = MPI.File.Open(MPI.COMM_SELF, filename, amode, MPI.INFO_NULL)
+            self.mpiobj = MPI.File.Open(MPI.COMM_SELF, filename, amode, MPI.INFO_NULL)
         except NotImplementedError:
+            try:
+                os.remove(filename)
+            except OSError:
+                pass
             self.skipTest('mpi-file')
-        file.Set_errhandler(MPI.ERRORS_RETURN)
-        #file.Call_errhandler(MPI.ERR_OTHER)
-        file.Call_errhandler(MPI.SUCCESS)
-        file.Close()
+
+    def tearDown(self):
+        self.mpiobj.Close()
+
+    @unittest.skipMPI('msmpi')
+    def testCall(self):
+        super().testCall()
 
 
-try:
-    MPI.Win.Create(MPI.BOTTOM, 1, MPI.INFO_NULL, MPI.COMM_SELF).Free()
-except (NotImplementedError, MPI.Exception):
-    TestErrhandler.testWinCallErrhandler = \
-    unittest.disable(TestErrhandler.testWinCallErrhandler, 'mpi-win')
+class TestErrhandlerSession(BaseTestErrhandler, unittest.TestCase):
+
+    def setUp(self):
+        try:
+            self.mpiobj = MPI.Session.Init()
+        except NotImplementedError:
+            self.skipTest('mpi-session')
+
+    def tearDown(self):
+        self.mpiobj.Finalize()
 
 
 if __name__ == '__main__':
