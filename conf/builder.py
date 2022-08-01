3a747ff108c4c1c596481fe456f50eb52482c616
Lisandro Dalcin <dalcinl@gmail.com>
2022-08-02 00:52:40 +0300

    build: Add support for build_editable hook (PEP 660)

--- conf/builder.py
+++ conf/builder.py
@@ -16,24 +16,10 @@ def get_backend_name(default='default'):
     ).lower().replace('_', '-')
 
 
-@contextlib.contextmanager
-def set_build_backend(backend='default'):
-    backend_saved = os.environ.get('MPI4PY_BUILD_BACKEND')
-    try:
-        os.environ['MPI4PY_BUILD_BACKEND'] = backend
-        yield
-    finally:
-        if backend_saved is None:
-            del os.environ['MPI4PY_BUILD_BACKEND']
-        else:
-            os.environ['MPI4PY_BUILD_BACKEND'] = backend_saved
-
-
-def get_requires_for_build_wheel(config_settings=None):
-    backend = get_backend_name()
-    use_skbuild = backend in ('scikit-build', 'skbuild')
-    with set_build_backend():
-        requires = _st_bm.get_requires_for_build_wheel(config_settings)
+def get_build_requires():
+    name = get_backend_name()
+    use_skbuild = name in ('scikit-build', 'skbuild')
+    requires = []
     requires.append(CYTHON)
     if use_skbuild:
         requires.append(SKBUILD)
@@ -42,9 +28,36 @@ def get_requires_for_build_wheel(config_settings=None):
     return requires
 
 
+@contextlib.contextmanager
+def set_build_backend(name='default'):
+    name_saved = os.environ.get('MPI4PY_BUILD_BACKEND')
+    try:
+        os.environ['MPI4PY_BUILD_BACKEND'] = name
+        yield _st_bm
+    finally:
+        if name_saved is None:
+            del os.environ['MPI4PY_BUILD_BACKEND']
+        else:
+            os.environ['MPI4PY_BUILD_BACKEND'] = name_saved
+
+
 def get_requires_for_build_sdist(config_settings=None):
-    with set_build_backend():
-        requires = _st_bm.get_requires_for_build_sdist(config_settings)
+    with set_build_backend() as backend:
+        requires = backend.get_requires_for_build_sdist(config_settings)
+    return requires
+
+
+def get_requires_for_build_wheel(config_settings=None):
+    with set_build_backend() as backend:
+        requires = backend.get_requires_for_build_wheel(config_settings)
+    requires += get_build_requires()
+    return requires
+
+
+def get_requires_for_build_editable(config_settings=None):
+    with set_build_backend() as backend:
+        requires = backend.get_requires_for_build_editable(config_settings)
+    requires += get_build_requires()
     return requires
 
 
@@ -52,9 +65,31 @@ def prepare_metadata_for_build_wheel(
     metadata_directory,
     config_settings=None,
 ):
-    with set_build_backend():
-        return _st_bm.prepare_metadata_for_build_wheel(
+    with set_build_backend() as backend:
+        return backend.prepare_metadata_for_build_wheel(
             metadata_directory,
+            config_settings,
+        )
+
+
+def prepare_metadata_for_build_editable(
+    metadata_directory,
+    config_settings=None,
+):
+    with set_build_backend() as backend:
+        return backend.prepare_metadata_for_build_editable(
+            metadata_directory,
+            config_settings,
+        )
+
+
+def build_sdist(
+    sdist_directory,
+    config_settings=None,
+):
+    with set_build_backend() as backend:
+        return backend.build_sdist(
+            sdist_directory,
             config_settings,
         )
 
@@ -64,21 +99,34 @@ def build_wheel(
     config_settings=None,
     metadata_directory=None,
 ):
-    backend = get_backend_name()
-    with set_build_backend(backend):
-        return _st_bm.build_wheel(
+    name = get_backend_name()
+    with set_build_backend(name) as backend:
+        return backend.build_wheel(
             wheel_directory,
             config_settings,
             metadata_directory,
         )
 
 
-def build_sdist(
-    sdist_directory,
+def build_editable(
+    wheel_directory,
     config_settings=None,
+    metadata_directory=None,
 ):
-    with set_build_backend():
-        return _st_bm.build_sdist(
-            sdist_directory,
+    name = 'default'  # TODO: support scikit-build
+    with set_build_backend(name) as backend:
+        return backend.build_editable(
+            wheel_directory,
             config_settings,
+            metadata_directory,
         )
+
+
+if not hasattr(_st_bm, 'get_requires_for_build_editable'):
+    del get_requires_for_build_editable
+
+if not hasattr(_st_bm, 'prepare_metadata_for_build_editable'):
+    del prepare_metadata_for_build_editable
+
+if not hasattr(_st_bm, 'build_editable'):
+    del build_editable
