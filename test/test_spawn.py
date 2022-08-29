17b895c532c793714ab23d0d69c99f08246c8875
Lisandro Dalcin <dalcinl@gmail.com>
2022-08-29 09:37:17 +0300

    test: Refactor spawn tests to split `Spawn()` from `Spawn_multiple()`

--- test/test_spawn.py
+++ test/test_spawn.py
@@ -17,8 +17,8 @@ def childscript():
     os.close(fd)
     with open(script, "w") as f:
         f.write(dedent("""\
-        #!%(python)s
-        import sys; sys.path.insert(0, "%(path)s")
+        #!{python}
+        import sys; sys.path.insert(0, "{path}")
         from mpi4py import MPI
         parent = MPI.Comm.Get_parent()
         parent.Barrier()
@@ -26,7 +26,7 @@ def childscript():
         assert parent == MPI.COMM_NULL
         parent = MPI.Comm.Get_parent()
         assert parent == MPI.COMM_NULL
-        """ % dict(
+        """.format(
             python=sys.executable,
             path=MPI4PYPATH,
         )))
@@ -85,10 +85,15 @@ class BaseTestSpawn(object):
     INFO = MPI.INFO_NULL
     ROOT = 0
 
+
+class BaseTestSpawnSingle(BaseTestSpawn):
+
     def testCommSpawn(self):
         self.COMM.Barrier()
-        child = self.COMM.Spawn(self.COMMAND, self.ARGS, self.MAXPROCS,
-                                info=self.INFO, root=self.ROOT)
+        child = self.COMM.Spawn(
+            self.COMMAND, self.ARGS, self.MAXPROCS,
+            info=self.INFO, root=self.ROOT,
+        )
         local_size = child.Get_size()
         remote_size = child.Get_remote_size()
         child.Barrier()
@@ -101,9 +106,11 @@ class BaseTestSpawn(object):
     def testErrcodes(self):
         self.COMM.Barrier()
         errcodes = []
-        child = self.COMM.Spawn(self.COMMAND, self.ARGS, self.MAXPROCS,
-                                info=self.INFO, root=self.ROOT,
-                                errcodes=errcodes)
+        child = self.COMM.Spawn(
+            self.COMMAND, self.ARGS, self.MAXPROCS,
+            info=self.INFO, root=self.ROOT,
+            errcodes=errcodes,
+        )
         child.Barrier()
         child.Disconnect()
         self.COMM.Barrier()
@@ -116,11 +123,15 @@ class BaseTestSpawn(object):
     def testArgsOnlyAtRoot(self):
         self.COMM.Barrier()
         if self.COMM.Get_rank() == self.ROOT:
-            child = self.COMM.Spawn(self.COMMAND, self.ARGS, self.MAXPROCS,
-                                    info=self.INFO, root=self.ROOT)
+            child = self.COMM.Spawn(
+                self.COMMAND, self.ARGS, self.MAXPROCS,
+                info=self.INFO, root=self.ROOT,
+            )
         else:
-            child = self.COMM.Spawn(None, None, -1,
-                                    info=MPI.INFO_NULL, root=self.ROOT)
+            child = self.COMM.Spawn(
+                None, None, -1,
+                info=MPI.INFO_NULL, root=self.ROOT,
+            )
         child.Barrier()
         child.Disconnect()
         self.COMM.Barrier()
@@ -133,8 +144,10 @@ class BaseTestSpawn(object):
             script = childscript()
         self.COMM.Barrier()
         script = self.COMM.bcast(script, root=self.ROOT)
-        child = self.COMM.Spawn(script, None, self.MAXPROCS,
-                                info=self.INFO, root=self.ROOT)
+        child = self.COMM.Spawn(
+            script, None, self.MAXPROCS,
+            info=self.INFO, root=self.ROOT,
+        )
         child.Barrier()
         child.Disconnect()
         self.COMM.Barrier()
@@ -142,7 +155,10 @@ class BaseTestSpawn(object):
             os.remove(script)
         self.COMM.Barrier()
 
-    def testCommSpawnMultiple(self):
+
+class BaseTestSpawnMultiple(BaseTestSpawn):
+
+    def testCommSpawn(self):
         self.COMM.Barrier()
         count = 2 + (self.COMM.Get_size() == 0)
         COMMAND = [self.COMMAND] * count
@@ -151,7 +167,8 @@ class BaseTestSpawn(object):
         INFO = [self.INFO] * len(COMMAND)
         child = self.COMM.Spawn_multiple(
             COMMAND, ARGS, MAXPROCS,
-            info=INFO, root=self.ROOT)
+            info=INFO, root=self.ROOT,
+        )
         local_size = child.Get_size()
         remote_size = child.Get_remote_size()
         child.Barrier()
@@ -160,7 +177,7 @@ class BaseTestSpawn(object):
         self.assertEqual(local_size, self.COMM.Get_size())
         self.assertEqual(remote_size, sum(MAXPROCS))
 
-    def testCommSpawnMultipleDefaults1(self):
+    def testCommSpawnDefaults1(self):
         self.COMM.Barrier()
         count = 2 + (self.COMM.Get_size() == 0)
         COMMAND = [self.COMMAND] * count
@@ -174,7 +191,7 @@ class BaseTestSpawn(object):
         self.assertEqual(local_size, self.COMM.Get_size())
         self.assertEqual(remote_size, len(COMMAND))
 
-    def testCommSpawnMultipleDefaults2(self):
+    def testCommSpawnDefaults2(self):
         self.COMM.Barrier()
         count = 2 + (self.COMM.Get_size() == 0)
         COMMAND = [self.COMMAND] * count
@@ -189,7 +206,7 @@ class BaseTestSpawn(object):
         self.assertEqual(remote_size, len(COMMAND))
 
     @unittest.skipMPI('msmpi')
-    def testErrcodesMultiple(self):
+    def testErrcodes(self):
         self.COMM.Barrier()
         count = 2 + (self.COMM.Get_size() == 0)
         COMMAND = [self.COMMAND] * count
@@ -200,7 +217,8 @@ class BaseTestSpawn(object):
         child = self.COMM.Spawn_multiple(
             COMMAND, ARGS, MAXPROCS,
             info=INFO, root=self.ROOT,
-            errcodes=errcodelist)
+            errcodes=errcodelist,
+        )
         child.Barrier()
         child.Disconnect()
         self.COMM.Barrier()
@@ -211,7 +229,7 @@ class BaseTestSpawn(object):
                 self.assertEqual(errcode, MPI.SUCCESS)
 
     @unittest.skipMPI('msmpi')
-    def testArgsOnlyAtRootMultiple(self):
+    def testArgsOnlyAtRoot(self):
         self.COMM.Barrier()
         if self.COMM.Get_rank() == self.ROOT:
             count = 2 + (self.COMM.Get_size() == 0)
@@ -221,17 +239,19 @@ class BaseTestSpawn(object):
             INFO = [MPI.INFO_NULL] * len(COMMAND)
             child = self.COMM.Spawn_multiple(
                 COMMAND, ARGS, MAXPROCS,
-                info=INFO, root=self.ROOT)
+                info=INFO, root=self.ROOT,
+            )
         else:
             child = self.COMM.Spawn_multiple(
                 None, None, -1,
-                info=MPI.INFO_NULL, root=self.ROOT)
+                info=MPI.INFO_NULL, root=self.ROOT,
+            )
         child.Barrier()
         child.Disconnect()
         self.COMM.Barrier()
 
     @unittest.skipIf(os.name != 'posix', 'posix')
-    def testNoArgsMultiple(self):
+    def testNoArgs(self):
         self.COMM.Barrier()
         script = None
         if self.COMM.Get_rank() == self.ROOT:
@@ -242,8 +262,10 @@ class BaseTestSpawn(object):
         COMMAND = [script] * count
         MAXPROCS = list(range(1, len(COMMAND)+1))
         INFO = [self.INFO] * len(COMMAND)
-        child = self.COMM.Spawn_multiple(COMMAND, None, MAXPROCS,
-                                         info=INFO, root=self.ROOT)
+        child = self.COMM.Spawn_multiple(
+            COMMAND, None, MAXPROCS,
+            info=INFO, root=self.ROOT,
+        )
         child.Barrier()
         child.Disconnect()
         self.COMM.Barrier()
@@ -251,18 +273,30 @@ class BaseTestSpawn(object):
             os.remove(script)
         self.COMM.Barrier()
 
-class TestSpawnSelf(BaseTestSpawn, unittest.TestCase):
+
+class TestSpawnSingleSelf(BaseTestSpawnSingle, unittest.TestCase):
     COMM = MPI.COMM_SELF
 
-class TestSpawnWorld(BaseTestSpawn, unittest.TestCase):
+class TestSpawnSingleWorld(BaseTestSpawnSingle, unittest.TestCase):
     COMM = MPI.COMM_WORLD
 
-class TestSpawnSelfMany(BaseTestSpawn, unittest.TestCase):
-    COMM = MPI.COMM_SELF
+class TestSpawnSingleSelfMany(TestSpawnSingleSelf):
     MAXPROCS = MPI.COMM_WORLD.Get_size()
 
-class TestSpawnWorldMany(BaseTestSpawn, unittest.TestCase):
+class TestSpawnSingleWorldMany(TestSpawnSingleWorld):
+    MAXPROCS = MPI.COMM_WORLD.Get_size()
+
+
+class TestSpawnMultipleSelf(BaseTestSpawnMultiple, unittest.TestCase):
+    COMM = MPI.COMM_SELF
+
+class TestSpawnMultipleWorld(BaseTestSpawnMultiple, unittest.TestCase):
     COMM = MPI.COMM_WORLD
+
+class TestSpawnMultipleSelfMany(TestSpawnMultipleSelf):
+    MAXPROCS = MPI.COMM_WORLD.Get_size()
+
+class TestSpawnMultipleWorldMany(TestSpawnMultipleWorld):
     MAXPROCS = MPI.COMM_WORLD.Get_size()
 
 
