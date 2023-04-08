1c453c4650b2b96268ba7bd9fdf584354ae6ba3d
Lisandro Dalcin <dalcinl@gmail.com>
2023-04-08 20:13:43 +0300

    build: Improve support for skbuild/mesonpy build backends

--- conf/builder.py
+++ conf/builder.py
@@ -5,6 +5,180 @@ import shutil
 
 # ---
 
+BACKENDS = {
+    'setuptools': 'setuptools.build_meta',
+    'skbuild': 'scikit_build_core.setuptools.build_meta',
+    'mesonpy': 'mesonpy',
+}
+
+
+def get_build_backend_name(name=None):
+    if name is None:
+        name = os.environ.get('MPI4PY_BUILD_BACKEND', '')
+        name = name.lower().replace('_', '-')
+    if name in ('default', ''):
+        return 'setuptools'
+    if name in ('setuptools', 'setup'):
+        return 'setuptools'
+    if name in ('scikit-build-core', 'scikit-build', 'skbuild', 'cmake'):
+        return 'skbuild'
+    if name in ('meson-python', 'mesonpy', 'meson'):
+        return 'mesonpy'
+    raise RuntimeError(f"Unknown build backend {name!r}")
+
+
+def build_backend(name=None):
+    name = get_build_backend_name(name)
+    return importlib.import_module(BACKENDS[name])
+
+
+def read_build_requires(name):
+    confdir = os.path.dirname(__file__)
+    basename = f'requirements-build-{name}.txt'
+    filename = os.path.join(confdir, basename)
+    with open(filename, encoding='utf-8') as f:
+        return [req for req in map(str.strip, f) if req]
+
+
+def get_backend_requires_fast(backend, dist, config_settings=None):
+    if backend is None or isinstance(backend, str):
+        try:
+            backend = build_backend(backend)
+        except ImportError:
+            return None
+    requires = []
+    get_requires = getattr(backend, f'get_requires_for_build_{dist}', None)
+    if get_requires is not None:
+        requires += get_requires(config_settings)
+    return requires
+
+
+def get_backend_requires_hook(name, dist, config_settings=None):
+    from build.env import IsolatedEnvBuilder
+    try:
+        from pyproject_hooks import BuildBackendHookCaller
+    except ImportError:
+        from pep517.wrappers import Pep517HookCaller as BuildBackendHookCaller
+
+    @contextlib.contextmanager
+    def environment(scripts_dir):
+        os_path = os.environ.get('PATH')
+        paths = os_path.split(os.pathsep) if os_path else []
+        while scripts_dir in paths:
+            paths.remove(scripts_dir)
+        paths.insert(0, scripts_dir)
+        os.environ['PATH'] = os.pathsep.join(paths)
+        build_backend = os.environ.pop('PEP517_BUILD_BACKEND', None)
+        backend_path = os.environ.pop('PEP517_BACKEND_PATH', None)
+        try:
+            yield None
+        finally:
+            if os_path is not None:
+                os.environ['PATH'] = os_path
+            if build_backend is not None:
+                os.environ['PEP517_BUILD_BACKEND'] = build_backend
+            if backend_path is not None:
+                os.environ['PEP517_BACKEND_PATH'] = backend_path
+
+    requires = read_build_requires(name)
+    with IsolatedEnvBuilder() as env:
+        with environment(env.scripts_dir):
+            env.install(requires)
+            hook = BuildBackendHookCaller(
+                source_dir=os.getcwd(),
+                build_backend=BACKENDS[name],
+                python_executable=env.executable,
+            )
+            requires += get_backend_requires_fast(hook, dist, config_settings)
+    return requires
+
+
+def get_requires_for_build(dist, config_settings=None):
+    name = get_build_backend_name()
+    requires = get_backend_requires_fast(name, dist, config_settings)
+    if requires is None:
+        requires = get_backend_requires_hook(name, dist, config_settings)
+    if dist in ('wheel', 'editable'):
+        requires += read_build_requires('cython')
+    return requires
+
+
+# ---
+
+
+def get_requires_for_build_sdist(config_settings=None):
+    return get_requires_for_build('sdist', config_settings)
+
+
+def build_sdist(
+    sdist_directory,
+    config_settings=None,
+):
+    return build_backend().build_sdist(
+        sdist_directory,
+        config_settings,
+    )
+
+
+# ---
+
+
+def get_requires_for_build_wheel(config_settings=None):
+    return get_requires_for_build('wheel', config_settings)
+
+
+def prepare_metadata_for_build_wheel(
+    metadata_directory,
+    config_settings=None,
+):
+    return build_backend().prepare_metadata_for_build_wheel(
+        metadata_directory,
+        config_settings,
+    )
+
+
+def build_wheel(
+    wheel_directory,
+    config_settings=None,
+    metadata_directory=None,
+):
+    return build_backend().build_wheel(
+        wheel_directory,
+        config_settings,
+        metadata_directory,
+    )
+
+
+# ---
+
+
+def get_requires_for_build_editable(config_settings=None):
+    return get_requires_for_build('editable', config_settings)
+
+
+def prepare_metadata_for_build_editable(
+    metadata_directory,
+    config_settings=None,
+):
+    return build_backend().prepare_metadata_for_build_editable(
+        metadata_directory,
+        config_settings,
+    )
+
+
+def build_editable(
+    wheel_directory,
+    config_settings=None,
+    metadata_directory=None,
+):
+    return build_backend().build_editable(
+        wheel_directory,
+        config_settings,
+        metadata_directory,
+    )
+
+# ---
+
 
 def setup_env_mpicc():
     mpicc = shutil.which('mpicc')
@@ -15,161 +189,10 @@ def setup_env_mpicc():
     if ' ' in mpicc:
         mpicc = f'"{mpicc}"'
     if 'CC' not in os.environ:
-        os.environ['CC'] =  mpicc
+        os.environ['CC'] = mpicc
 
 
-# ---
-
-def get_backend_name(default='default'):
-    name = os.environ.get(
-        'MPI4PY_BUILD_BACKEND', default,
-    ).lower().replace('_', '-')
-    if name in ('default', ''):
-        return 'setuptools'
-    if name in ('setuptools', 'setup'):
-        return 'setuptools'
-    if name in ('scikit-build', 'skbuild', 'cmake'):
-        return 'scikit-build'
-    if name in ('meson-python', 'mesonpy', 'meson'):
-        return 'meson-python'
-    raise RuntimeError(f"Unknown build backend {name!r}")
-
-
-def read_build_requires(package):
-    confdir = os.path.dirname(__file__)
-    basename = f'requirements-build-{package}.txt'
-    with open(os.path.join(confdir, basename)) as f:
-        return [req for req in map(str.strip, f) if req]
-
-
-def get_requires_for_build(dist):
-    name = get_backend_name()
-    requires = []
-    if dist == 'wheel':
-        if name == 'scikit-build':
-            requires += read_build_requires('skbuild')
-        if name == 'meson-python':
-            requires += read_build_requires('mesonpy')
-    if dist in ('wheel', 'editable'):
-        requires += read_build_requires('cython')
-    return requires
-
-
-@contextlib.contextmanager
-def build_backend(name='default'):
-    name_saved = os.environ.get('MPI4PY_BUILD_BACKEND')
-    os.environ['MPI4PY_BUILD_BACKEND'] = name
-    try:
-        name = get_backend_name()
-        if name == 'setuptools':
-            yield importlib.import_module('setuptools.build_meta')
-        if name == 'scikit-build':
-            yield importlib.import_module('setuptools.build_meta')
-        if name == 'meson-python':
-            setup_env_mpicc()
-            yield importlib.import_module('mesonpy')
-    finally:
-        if name_saved is None:
-            del os.environ['MPI4PY_BUILD_BACKEND']
-        else:
-            os.environ['MPI4PY_BUILD_BACKEND'] = name_saved
-
-
-# ---
-
-
-def get_requires_for_build_sdist(config_settings=None):
-    with build_backend() as backend:
-        requires = backend.get_requires_for_build_sdist(config_settings)
-    requires += get_requires_for_build('sdist')
-    return requires
-
-
-def build_sdist(
-    sdist_directory,
-    config_settings=None,
-):
-    with build_backend() as backend:
-        return backend.build_sdist(
-            sdist_directory,
-            config_settings,
-        )
-
-
-# ---
-
-
-def get_requires_for_build_wheel(config_settings=None):
-    with build_backend() as backend:
-        requires = backend.get_requires_for_build_wheel(config_settings)
-    requires += get_requires_for_build('wheel')
-    return requires
-
-
-def prepare_metadata_for_build_wheel(
-    metadata_directory,
-    config_settings=None,
-):
-    name = get_backend_name()
-    with build_backend(name) as backend:
-        return backend.prepare_metadata_for_build_wheel(
-            metadata_directory,
-            config_settings,
-        )
-
-
-def build_wheel(
-    wheel_directory,
-    config_settings=None,
-    metadata_directory=None,
-):
-    name = get_backend_name()
-    with build_backend(name) as backend:
-        return backend.build_wheel(
-            wheel_directory,
-            config_settings,
-            metadata_directory,
-        )
-
-
-# ---
-
-
-def get_requires_for_build_editable(config_settings=None):
-    with build_backend() as backend:
-        requires = backend.get_requires_for_build_editable(config_settings)
-    requires += get_requires_for_build('editable')
-    return requires
-
-
-def prepare_metadata_for_build_editable(
-    metadata_directory,
-    config_settings=None,
-):
-    name = 'setuptools'  # TODO: support scikit-build
-    with build_backend(name) as backend:
-        return backend.prepare_metadata_for_build_editable(
-            metadata_directory,
-            config_settings,
-        )
-
-
-def build_editable(
-    wheel_directory,
-    config_settings=None,
-    metadata_directory=None,
-):
-    name = 'setuptools'  # TODO: support scikit-build
-    with build_backend(name) as backend:
-        return backend.build_editable(
-            wheel_directory,
-            config_settings,
-            metadata_directory,
-        )
-
-# ---
-
-if get_backend_name() == 'setuptools':
+if get_build_backend_name() == 'setuptools':
     try:
         import setuptools.build_meta as st_bm
     except ImportError:
@@ -182,7 +205,13 @@ if get_backend_name() == 'setuptools':
         del build_editable
     del st_bm
 
-if get_backend_name() == 'meson-python':
+if get_build_backend_name() == 'skbuild':
+    del get_requires_for_build_editable
+    del prepare_metadata_for_build_editable
+    del build_editable
+
+if get_build_backend_name() == 'mesonpy':
+    setup_env_mpicc()
     del prepare_metadata_for_build_wheel
     del get_requires_for_build_editable
     del prepare_metadata_for_build_editable
