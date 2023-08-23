b3dc2ce2af777ebb43cff7f912b7836e21ccb1fc
Lisandro Dalcin <dalcinl@gmail.com>
2023-08-23 20:35:51 +0300

    build: Monkeypatch distutils to support SOURCE_DATE_EPOCH for sdist

--- conf/mpidistutils.py
+++ conf/mpidistutils.py
@@ -1590,3 +1590,63 @@ if setuptools:
         mod_egg_info.FileList = FileList
 
 # -----------------------------------------------------------------------------
+
+# Support for Reproducible Builds
+# https://reproducible-builds.org/docs/source-date-epoch/
+
+timestamp = os.environ.get('SOURCE_DATE_EPOCH')
+if timestamp is not None:
+    import distutils.archive_util as archive_util
+    import stat
+    import tarfile
+    import time
+
+    timestamp = float(max(int(timestamp), 0))
+
+    class Time:
+        @staticmethod
+        def time():
+            return timestamp
+        @staticmethod
+        def localtime(_=None):
+            return time.localtime(timestamp)
+
+    class TarInfoMode:
+        def __get__(self, obj, objtype=None):
+            return obj._mode
+        def __set__(self, obj, stmd):
+            ifmt = stat.S_IFMT(stmd)
+            mode = stat.S_IMODE(stmd) & 0o7755
+            obj._mode = ifmt | mode
+
+    class TarInfoAttr:
+        def __init__(self, value):
+            self.value = value
+        def __get__(self, obj, objtype=None):
+            return self.value
+        def __set__(self, obj, value):
+            pass
+
+    class TarInfo(tarfile.TarInfo):
+        mode = TarInfoMode()
+        mtime = TarInfoAttr(timestamp)
+        uid = TarInfoAttr(1000)
+        gid = TarInfoAttr(100)
+        uname = TarInfoAttr('user')
+        gname = TarInfoAttr('users')
+
+    def make_tarball(*args, **kwargs):
+        tarinfo_orig = tarfile.TarFile.tarinfo
+        try:
+            tarfile.time = Time()
+            tarfile.TarFile.tarinfo = TarInfo
+            return archive_util.make_tarball(*args, **kwargs)
+        finally:
+            tarfile.time = time
+            tarfile.TarFile.tarinfo = tarinfo_orig
+
+    archive_util.ARCHIVE_FORMATS['gztar'] = (
+        make_tarball, *archive_util.ARCHIVE_FORMATS['gztar'][1:],
+    )
+
+# -----------------------------------------------------------------------------
