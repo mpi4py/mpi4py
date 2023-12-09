2cf4deea925e38561d2b5a9fb6af8cd68e342ff7
Lisandro Dalcin <dalcinl@gmail.com>
2023-12-09 10:14:27 -0300

    CI: Rework Open MPI tweaks to support v5.0.x

--- .azure/install-openmpi.sh
+++ .azure/install-openmpi.sh
@@ -11,14 +11,12 @@ esac
 
 openmpi_mca_params=$HOME/.openmpi/mca-params.conf
 mkdir -p $(dirname $openmpi_mca_params)
-echo plm=isolated >> $openmpi_mca_params
+echo plm_rsh_agent=false >> $openmpi_mca_params
 echo btl=tcp,self >> $openmpi_mca_params
+echo mpi_yield_when_idle=true >> $openmpi_mca_params
 echo rmaps_base_oversubscribe=true >> $openmpi_mca_params
 echo btl_base_warn_component_unused=false >> $openmpi_mca_params
 echo btl_vader_single_copy_mechanism=none >> $openmpi_mca_params
-if [[ `uname` == Darwin ]]; then
-    # open-mpi/ompi#7516
-    echo gds=hash >> $openmpi_mca_params
-    # open-mpi/ompi#5798
-    echo btl_vader_backing_directory=/tmp >> $openmpi_mca_params
-fi
+prte_mca_params=$HOME/.prte/mca-params.conf
+mkdir -p $(dirname $prte_mca_params)
+echo rmaps_default_mapping_policy=:oversubscribe >> $prte_mca_params
--- .circleci/env.sh
+++ .circleci/env.sh
@@ -6,11 +6,13 @@ export MPI4PY_COVERAGE_PLUGIN=cycoverage
 export PYTHONPATH=$PWD/conf
 
 export HYDRA_LAUNCHER=fork
-export OMPI_MCA_plm=isolated
+export OMPI_MCA_plm_rsh_agent=false
 export OMPI_MCA_btl=tcp,self
+echo   OMPI_MCA_mpi_yield_when_idle=true
 export OMPI_MCA_rmaps_base_oversubscribe=true
 export OMPI_MCA_btl_base_warn_component_unused=false
 export OMPI_MCA_btl_vader_single_copy_mechanism=none
+export PRTE_MCA_rmaps_default_mapping_policy=:oversubscribe
 export OMPI_ALLOW_RUN_AS_ROOT=1
 export OMPI_ALLOW_RUN_AS_ROOT_CONFIRM=1
 
