c7e91107bab64eec0edcad9c95b99f19d1362207
Lisandro Dalcin <dalcinl@gmail.com>
2024-08-20 11:41:27 +0300

    type: Disable errors from NumPy with unsupported Python versions

--- src/mpi4py/util/dtlib.pyi
+++ src/mpi4py/util/dtlib.pyi
@@ -3,5 +3,6 @@ from numpy import dtype
 from numpy.typing import DTypeLike
 from ..MPI import Datatype
 
+# mypy: disable-error-code="no-any-unimported"
 def from_numpy_dtype(dtype: DTypeLike) -> Datatype: ...
 def to_numpy_dtype(datatype: Datatype) -> dtype[Any]: ...
