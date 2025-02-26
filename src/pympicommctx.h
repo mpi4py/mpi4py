5baf163f3ebcb702bfa1da04f33c726853af092c
Lisandro Dalcin <dalcinl@gmail.com>
2025-02-26 20:36:07 +0300

    refactor: Don't assume MPI_KEYVAL_INVALID is a compile-time constant

--- src/pympicommctx.h
+++ src/pympicommctx.h
@@ -23,7 +23,7 @@ typedef struct {
   int      low_group;
 } PyMPI_Commctx;
 
-static int PyMPI_Commctx_KEYVAL = MPI_KEYVAL_INVALID;
+static int PyMPI_Commctx_KEYVAL = -1;
 static int PyMPI_Commctx_TAG_UB = -1;
 
 static int PyMPI_Commctx_new(PyMPI_Commctx **_commctx)
@@ -65,6 +65,7 @@ static int MPIAPI PyMPI_Commctx_free_fn(MPI_Comm comm, int k, void *v, void *xs)
 static int PyMPI_Commctx_keyval(int *keyval)
 {
   int ierr;
+  if (PyMPI_Commctx_KEYVAL == -1) PyMPI_Commctx_KEYVAL = MPI_KEYVAL_INVALID;
   if (PyMPI_Commctx_KEYVAL != MPI_KEYVAL_INVALID) goto fn_exit;
   ierr = MPI_Comm_create_keyval(MPI_COMM_NULL_COPY_FN,
                                 PyMPI_Commctx_free_fn,
@@ -99,6 +100,7 @@ static int PyMPI_Commctx_clear(MPI_Comm comm)
   int ierr, found = 0, keyval = PyMPI_Commctx_KEYVAL;
   PyMPI_Commctx *commctx = NULL;
 
+  if (keyval == -1) keyval = MPI_KEYVAL_INVALID;
   if (keyval == MPI_KEYVAL_INVALID) return MPI_SUCCESS;
   ierr = MPI_Comm_get_attr(comm, keyval, &commctx, &found); CHKERR(ierr);
   if (found) {ierr = MPI_Comm_delete_attr(comm, keyval); CHKERR(ierr);}
@@ -121,6 +123,7 @@ static int PyMPI_Commctx_inter(MPI_Comm comm, MPI_Comm *dupcomm, int *tag,
                                MPI_Comm *localcomm, int *low_group)
 {
   int ierr;
+  int version, subversion;
   PyMPI_Commctx *commctx = NULL;
   ierr = PyMPI_Commctx_lookup(comm, &commctx);CHKERR(ierr);
   if (commctx->localcomm == MPI_COMM_NULL) {
@@ -133,16 +136,15 @@ static int PyMPI_Commctx_inter(MPI_Comm comm, MPI_Comm *dupcomm, int *tag,
     commctx->low_group = ((localsize>remotesize) ? 0 :
                           (localsize<remotesize) ? 1 :
                           (mergerank<localsize));
-#if (MPI_VERSION > 2) || (MPI_VERSION == 2 && MPI_SUBVERSION >= 2)
-  {
-    MPI_Group localgroup = MPI_GROUP_NULL;
-    ierr = MPI_Comm_group(comm, &localgroup); CHKERR(ierr);
-    ierr = MPI_Comm_create(mergecomm, localgroup, &commctx->localcomm); CHKERR(ierr);
-    ierr = MPI_Group_free(&localgroup); CHKERR(ierr);
-  }
-#else
-    ierr = MPI_Comm_split(mergecomm, commctx->low_group, 0, &commctx->localcomm); CHKERR(ierr);
-#endif
+    ierr = MPI_Get_version(&version, &subversion);CHKERR(ierr);
+    if (version > 2 || (version == 2 && subversion >= 2)) {
+      MPI_Group localgroup = MPI_GROUP_NULL;
+      ierr = MPI_Comm_group(comm, &localgroup); CHKERR(ierr);
+      ierr = MPI_Comm_create(mergecomm, localgroup, &commctx->localcomm); CHKERR(ierr);
+      ierr = MPI_Group_free(&localgroup); CHKERR(ierr);
+    } else {
+      ierr = MPI_Comm_split(mergecomm, commctx->low_group, 0, &commctx->localcomm); CHKERR(ierr);
+    }
     ierr = MPI_Comm_free(&mergecomm); CHKERR(ierr);
   }
   if (dupcomm)
@@ -159,6 +161,7 @@ static int PyMPI_Commctx_inter(MPI_Comm comm, MPI_Comm *dupcomm, int *tag,
 static int PyMPI_Commctx_finalize(void)
 {
   int ierr;
+  if (PyMPI_Commctx_KEYVAL == -1) PyMPI_Commctx_KEYVAL = MPI_KEYVAL_INVALID;
   if (PyMPI_Commctx_KEYVAL == MPI_KEYVAL_INVALID) return MPI_SUCCESS;
   ierr = PyMPI_Commctx_clear(MPI_COMM_SELF); CHKERR(ierr);
   ierr = PyMPI_Commctx_clear(MPI_COMM_WORLD); CHKERR(ierr);
