d4aff5c6d7ca7ba73f416850f84976df9f9c4864
Lisandro Dalcin <dalcinl@gmail.com>
2025-03-06 00:00:57 +0300

    refactor: Add internal header to consolidate C includes in Cython code

---
+++ src/lib-mpi.h
@@ -0,0 +1,7 @@
+/* Author:  Lisandro Dalcin   */
+/* Contact: dalcinl@gmail.com */
+
+#include "lib-mpi/config.h"
+#include "lib-mpi/missing.h"
+#include "lib-mpi/fallback.h"
+#include "lib-mpi/compat.h"
