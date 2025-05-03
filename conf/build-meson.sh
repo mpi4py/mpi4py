0e4e74a2754948cab4c243e96c43128a2a735316
Lisandro Dalcin <dalcinl@gmail.com>
2025-05-03 12:17:16 +0300

    build: Require meson>=1.5.1 and simplify support

--- conf/build-meson.sh
+++ conf/build-meson.sh
@@ -4,7 +4,6 @@ test -f meson.build
 test -d src/mpi4py
 rm -rf build install
 options="--python.bytecompile=-1 --python.platlibdir="""
-env CC=mpicc \
 meson setup build --prefix="$PWD/install" $options "$@"
 meson compile -C build
 meson install -C build
