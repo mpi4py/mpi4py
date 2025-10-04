30f5553a568d3e3339edb25d50bc6a5f6bda665b
Lisandro Dalcin <dalcinl@gmail.com>
2025-10-04 15:56:35 +0300

    feat: Add additional informational options to command line
    * --module: extension module path
    * --mpi-vendor: MPI vendor name and version
    * --mpi-abi-version: MPI ABI version

--- cibw/run-tests.sh
+++ cibw/run-tests.sh
@@ -18,8 +18,11 @@ esac
 
 set -x
 python --version
-python -m mpi4py --prefix
 python -m mpi4py --version
+python -m mpi4py --prefix
+python -m mpi4py --module
+python -m mpi4py --mpi-vendor
 python -m mpi4py --mpi-library
+python -m mpi4py --mpi-abi-version
 python -m mpi4py --mpi-std-version
 python -m mpi4py --mpi-lib-version
