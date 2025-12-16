40f6ec0f5aa1f4bf4bb82c714b84b28f464a04c1
Lisandro Dalcin <dalcinl@gmail.com>
2025-12-16 22:02:37 +0300

    CD: Enable wheel tests with conda-forge openmpi 4.1

--- cibw/run-tests-conda.sh
+++ cibw/run-tests-conda.sh
@@ -7,8 +7,6 @@ openmpi=("5.0" "4.1")
 impi=("2021.17.0" "2021.10.0")
 msmpi=("10.1.1")
 
-openmpi=("5.0")  # conda-forge/openmpi-feedstock#153
-
 mpi="$1"
 mpipackage="$mpi"
 mpiversion="${mpi}[@]"
@@ -25,5 +23,5 @@ for version in "${!mpiversion}"; do
     "$CONDA_EXE" list
     "$CONDA_EXE" run bash "$scriptdir"/run-tests-mpi.sh
     echo "::endgroup::"
+    "$CONDA_EXE" uninstall -qy "$mpipackage"
 done
-"$CONDA_EXE" uninstall -qy "$mpipackage"
